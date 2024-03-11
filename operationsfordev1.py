94370286ceec94002e53d3fe22031427c0498539
abdulsayyed101 <115169245+abdulsayyed101@users.noreply.github.com>
2024-03-11 21:38:55 +0100

    Update operationsfordev1.py

--- operationsfordev1.py
+++ operationsfordev1.py
@@ -1,9 +1,8 @@
 
 name =  'Abdul Sayyed'
 age = 25 
-print(f' My {name} is Abdul Rahim and i am {age} years old')
+print(f' My name is {name} and i am {age} years old')
 print('My name is Abdul Sayyed')
 print('whats your name ?')
-
 print('i have aleady told you my name...')
 
